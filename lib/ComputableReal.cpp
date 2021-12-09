9a348b1b3f5bd1e1bb655cbbdebcb9dd956bf737
konayuki002 <konayuki0455@gmail.com>
2021-12-09 18:59:59 +0900

    Add comparition of computable real

--- lib/ComputableReal.cpp
+++ lib/ComputableReal.cpp
@@ -5,13 +5,50 @@ IntervalRational ComputableReal::interval() const
   return current_interval;
 }
 
-void ComputableReal::diminish_until(const std::function<bool(const IntervalRational &)> &condition)
+void ComputableReal::diminish()
 {
-  while (!condition(current_interval))
-  {
-    current_interval = recurrence_formula(current_interval);
-  }
+  current_interval = recurrence_formula(current_interval);
 }
+
+bool unsafe_less_than(ComputableReal &cr1, ComputableReal &cr2)
+{
+  while (!(cr1.interval() < cr2.interval()).determined())
+  {
+    cr1.diminish();
+    cr2.diminish();
+  }
+
+  return (cr1.interval() < cr2.interval()).get_value();
+}
+
+bool unsafe_greater_than(ComputableReal &cr1, ComputableReal &cr2)
+{
+  return unsafe_less_than(cr2, cr1);
+}
+
+bool unsafe_less_than_or_equal_to(ComputableReal &cr1, ComputableReal &cr2)
+{
+  return !unsafe_greater_than(cr1, cr2);
+}
+
+bool unsafe_greater_than_or_equal_to(ComputableReal &cr1, ComputableReal &cr2)
+{
+  return unsafe_less_than_or_equal_to(cr2, cr1);
+}
+
+bool operator==(const ComputableReal &cr1, const ComputableReal &cr2)
+{
+  MaybeBool equality = (cr1.interval() == cr2.interval());
+
+  if (equality.determined())
+  {
+    return equality.get_value();
+  }
+  else
+  {
+    throw std::domain_error("Equality of computable real number with non-zero width cannot be determined");
+  }
+};
 
 std::ostream &operator<<(std::ostream &os, const ComputableReal &cr)
 {
--- test/ComputableRealTest.cpp
+++ test/ComputableRealTest.cpp
@@ -21,7 +21,7 @@ TEST(ComputableRealTest, Interval)
   EXPECT_EQ(ivr_pair.to_pair().second, 1);
 }
 
-TEST(ComputableRealTest, DiminishWhile)
+TEST(ComputableRealTest, Diminish)
 {
   using namespace alias::rational;
 
@@ -29,14 +29,70 @@ TEST(ComputableRealTest, DiminishWhile)
 
   ComputableReal cr_halving(IntervalRational(0, 1), halve_f);
 
-  cr_halving.diminish_until([](const IntervalRational &ivr) {
-    auto ivr_pair = ivr.to_pair();
-    return 0 <= ivr_pair.first && ivr_pair.second <= 1_r / 1000;
-  });
+  cr_halving.diminish();
 
   auto ivr_pair = cr_halving.interval().to_pair();
   EXPECT_EQ(ivr_pair.first, 0);
-  EXPECT_EQ(ivr_pair.second, 1_r / 1024);
+  EXPECT_EQ(ivr_pair.second, 1_r / 2);
+}
+
+TEST(ComputableRealTest, UnsafeLessThan)
+{
+  using namespace alias::rational;
+
+  std::function<IntervalRational(const IntervalRational &ivr)> halve_f = halve;
+
+  ComputableReal cr_halving(IntervalRational(0, 1), halve_f);
+
+  auto cr_halving_to_1 = ComputableReal(IntervalRational(0, 1), [](const IntervalRational &ivr) { return (ivr - 1) * 1_r / 2 + 1; });
+
+  EXPECT_TRUE(unsafe_less_than(cr_halving, cr_halving_to_1));
+}
+
+TEST(ComputableRealTest, UnsafeGreaterThan)
+{
+  using namespace alias::rational;
+
+  std::function<IntervalRational(const IntervalRational &ivr)> halve_f = halve;
+
+  ComputableReal cr_halving(IntervalRational(0, 1), halve_f);
+
+  auto cr_halving_to_1 = ComputableReal(IntervalRational(0, 1), [](const IntervalRational &ivr) { return (ivr - 1) * 1_r / 2 + 1; });
+
+  EXPECT_TRUE(unsafe_greater_than(cr_halving_to_1, cr_halving));
+}
+
+TEST(ComputableRealTest, UnsafeLessThanOrEqualTo)
+{
+  using namespace alias::rational;
+
+  std::function<IntervalRational(const IntervalRational &ivr)> halve_f = halve;
+
+  ComputableReal cr_halving(IntervalRational(0, 1), halve_f);
+
+  auto cr_halving_to_1 = ComputableReal(IntervalRational(0, 1), [](const IntervalRational &ivr) { return (ivr - 1) * 1_r / 2 + 1; });
+
+  EXPECT_TRUE(unsafe_less_than_or_equal_to(cr_halving, cr_halving_to_1));
+}
+
+TEST(ComputableRealTest, UnsafeGreaterThanEqualTo)
+{
+  using namespace alias::rational;
+
+  std::function<IntervalRational(const IntervalRational &ivr)> halve_f = halve;
+
+  ComputableReal cr_halving(IntervalRational(0, 1), halve_f);
+
+  auto cr_halving_to_1 = ComputableReal(IntervalRational(0, 1), [](const IntervalRational &ivr) { return (ivr - 1) * 1_r / 2 + 1; });
+
+  EXPECT_TRUE(unsafe_greater_than_or_equal_to(cr_halving_to_1, cr_halving));
+}
+
+TEST(ComputableRealTest, Equality)
+{
+  EXPECT_TRUE(ComputableReal(0) == ComputableReal(0));
+  EXPECT_FALSE(ComputableReal(0) == ComputableReal(1));
+  EXPECT_THROW(ComputableReal(IntervalRational(0, 1), [](const IntervalRational &ivr) { return ivr; }) == ComputableReal(0), std::domain_error);
 }
 
 TEST(ComputableRealTest, OutputStream)
