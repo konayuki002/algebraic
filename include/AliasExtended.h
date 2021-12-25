40e24344bcf1310a4571c49c489c4067725b9b7c
Yukihide Ochi <konayuki0455@gmail.com>
2021-12-25 16:50:18 +0900

    Remove alias of user literal

--- include/AliasExtended.h
+++ include/AliasExtended.h
@@ -6,6 +6,4 @@
 namespace alias::extended::rational
 {
   static const Extended<Rational> oo = Extended<Rational>(Infinity::PositiveInfinity);
-
-  static Extended<Rational> operator"" _exr(const unsigned long long n) { return Extended<Rational>(n); }
 } // namespace alias::extended::rational
